19d38394b0c22a7361717b8a9c06a1e22af4922d
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-28 19:33:17 -0300

    add aula image

--- 1_widths_stateless_statteful/nosso_primeiro_projeto/lib/main.dart
+++ 1_widths_stateless_statteful/nosso_primeiro_projeto/lib/main.dart
@@ -13,16 +13,134 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Geeks for geeks'),
+          title: Text('Tarefas'),
         ),
-        body: Container(
-          child: Center(child: Text('Hei')),
+        body: ListView(
+          children: [
+            Task(
+                'Aprender Flutter',
+                'https://imgs.search.brave.com/u_EApJWCpD-'
+                    'Ga3MMsUf1m1o02p2HCY6zDS6UzlozKc8/rs:fit:431:225:1/g:ce/aHR0cHM'
+                    '6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5S/a0pNeUo1TFB3cjZE/R'
+                    'UowaUUzRGR3SGFJ/SiZwaWQ9QXBp'),
+            Task('Meditar', ''),
+            Task('Ler', ''),
+            Task('Andar de bike', ''),
+            Task('Meditar', ''),
+          ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class Task extends StatefulWidget {
+  final String nome;
+  final String foto;
+
+  const Task(this.nome, this.foto, {Key? key}) : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                        child: Image.network(
+                          widget.foto,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: TextStyle(
+                                fontSize: 24, overflow: TextOverflow.ellipsis),
+                          )),
+                      Container(
+                        height: 52,
+                        width: 52,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
+                              print(nivel);
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(Icons.arrow_drop_up),
+                                Text(
+                                  'UP',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: nivel / 10,
+                        ),
+                        width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text(
+                        'Nível :$nivel'
+                        '',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
