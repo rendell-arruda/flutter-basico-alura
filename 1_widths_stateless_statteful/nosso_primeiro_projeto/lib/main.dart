d88e2f744c4791667d59c3b7483809c618eff12b
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-29 10:21:48 -0300

    add aula de opacidade

--- 1_widths_stateless_statteful/nosso_primeiro_projeto/lib/main.dart
+++ 1_widths_stateless_statteful/nosso_primeiro_projeto/lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
+
+// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,33 +26,44 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tarefas'),
         ),
-        body: ListView(
-          children: [
-            Task(
-                'Aprender Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                3),
-            Task(
-                'Meditar',
-                'https://manhattanmentalhealthcounseling.com/wp'
-                    '-content/uploads/2019/06/Top-5-Scientific-Findings-on-'
-                    'MeditationMindfulness-881x710.jpeg',
-                5),
-            Task(
-                'Ler',
-                'https://thebogotapost.com/wp-content/uploads/'
-                    '2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-                4),
-            Task(
-                'Andar de bike',
-                'https://tswbike.com/wp-content/uploads/2020/09/108034687'
-                    '_626160478000800_2490880540739582681_n-e1600200953343.jpg',
-                1),
-            Task('Jogar',
-                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 3),
-          ],
+        body: AnimatedOpacity(
+          opacity: opacidade ? 1 : 0,
+          duration: Duration(milliseconds: 800),
+          child: ListView(
+            children: [
+              Task(
+                  'Aprender Flutter',
+                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                  3),
+              Task(
+                  'Meditar',
+                  'https://manhattanmentalhealthcounseling.com/wp'
+                      '-content/uploads/2019/06/Top-5-Scientific-Findings-on-'
+                      'MeditationMindfulness-881x710.jpeg',
+                  5),
+              Task(
+                  'Ler',
+                  'https://thebogotapost.com/wp-content/uploads/'
+                      '2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                  4),
+              Task(
+                  'Andar de bike',
+                  'https://tswbike.com/wp-content/uploads/2020/09/108034687'
+                      '_626160478000800_2490880540739582681_n-e1600200953343.jpg',
+                  1),
+              Task('Jogar',
+                  'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 3),
+            ],
+          ),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState((){
+              opacidade = !opacidade;
+            });
+          },
+          child: Icon(Icons.remove_red_eye),
+        ),
       ),
     );
   }
@@ -94,7 +112,8 @@ class _TaskState extends State<Task> {
                             borderRadius: BorderRadius.circular(5)),
                         width: 72,
                         height: 100,
-                        child: ClipRRect(borderRadius: BorderRadius.circular(5),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5),
                           child: Image.network(
                             widget.foto,
                             fit: BoxFit.cover,
@@ -200,7 +219,7 @@ class _TaskState extends State<Task> {
                       padding: const EdgeInsets.all(12.0),
                       child: Text(
                         'Nível :$nivel'
-                            '',
+                        '',
                         style: TextStyle(color: Colors.white, fontSize: 16),
                       ),
                     ),
