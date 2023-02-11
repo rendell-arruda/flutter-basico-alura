9f5df52e4029a2ad9bd46ea7990de404c0afdd11
Rendell Arruda <rendellarruda3@gmail.com>
2023-02-11 17:59:30 -0300

    validacao e keyboard

--- 1_widths_stateless_statteful/nosso_primeiro_projeto/lib/screens/form_screen.dart
+++ 1_widths_stateless_statteful/nosso_primeiro_projeto/lib/screens/form_screen.dart
@@ -126,6 +126,11 @@ class _FormScreenState extends State<FormScreen> {
                         print(nameController.text);
                         print(int.parse(difficultyController.text));
                         print(imageController.text);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Printando nova tarefa'),
+                          ),
+                        );
                       }
                     },
                     child: Text('Adicionar!'),
